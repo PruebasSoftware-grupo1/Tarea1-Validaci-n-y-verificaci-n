f029dfbfd3b8b5758200fb232370d9a453d792b8
MartinAbbott97 <martin97abbott@gmail.com>
2023-04-09 18:11:59 -0400

    Define encode algorithm

--- client.py
+++ client.py
@@ -5,6 +5,46 @@ nickname = input("Choose a nickname: ")
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(('127.0.0.1',50000))
 
+def qwerty_positional_encode(message):
+    qwerty_map = {
+        'a': 'q', 'b': 'w', 'c': 'e', 'd': 'r', 'e': 't',
+        'f': 'y', 'g': 'u', 'h': 'i', 'i': 'o', 'j': 'p',
+        'k': 'a', 'l': 's', 'm': 'd', 'n': 'f', 'o': 'g',
+        'p': 'h', 'q': 'j', 'r': 'k', 's': 'l', 't': 'z',
+        'u': 'x', 'v': 'c', 'w': 'v', 'x': 'b', 'y': 'n',
+        'z': 'm', ' ': ' '
+    }
+    encoded_message = ''
+    for i, char in enumerate(message):
+        if char in qwerty_map:
+            shift = i + 1
+            qwerty_pos = list(qwerty_map.keys()).index(char)
+            shifted_pos = (qwerty_pos + shift) % len(qwerty_map)
+            encoded_message += qwerty_map[list(qwerty_map.keys())[shifted_pos]]
+        else:
+            encoded_message += char
+    return encoded_message
+
+def qwerty_positional_decode(encoded_message):
+    qwerty_map = {
+        'q': 'a', 'w': 'b', 'e': 'c', 'r': 'd', 't': 'e',
+        'y': 'f', 'u': 'g', 'i': 'h', 'o': 'i', 'p': 'j',
+        'a': 'k', 's': 'l', 'd': 'm', 'f': 'n', 'g': 'o',
+        'h': 'p', 'j': 'q', 'k': 'r', 'l': 's', 'z': 't',
+        'x': 'u', 'c': 'v', 'v': 'w', 'b': 'x', 'n': 'y',
+        'm': 'z', ' ': ' '
+    }
+    decoded_message = ''
+    for i, char in enumerate(encoded_message):
+        if char in qwerty_map:
+            shift = i + 1
+            qwerty_pos = list(qwerty_map.keys()).index(char)
+            shifted_pos = (qwerty_pos - shift) % len(qwerty_map)
+            decoded_message += qwerty_map[list(qwerty_map.keys())[shifted_pos]]
+        else:
+            decoded_message += char
+    return decoded_message
+
 def receive():
     while True:
         try:
