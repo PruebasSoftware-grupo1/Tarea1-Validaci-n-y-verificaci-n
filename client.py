ca532486b17199e7f537b74711af5768c1b169eb
Maxinho99 <maximiniabarca@gmail.com>
2023-04-10 01:31:25 -0400

    logs

--- client.py
+++ client.py
@@ -1,9 +1,54 @@
 import socket
 import threading
+import logging
+import time
+
+logging.basicConfig(filename='client.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
 
 nickname = input("Choose a nickname: ")
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(('127.0.0.1',50000))
+
+def qwerty_positional_encode(message):
+    qwerty_map = {
+        'a': 'q', 'b': 'w', 'c': 'e', 'd': 'r', 'e': 't',
+        'f': 'y', 'g': 'u', 'h': 'i', 'i': 'o', 'j': 'p',
+        'k': 'a', 'l': 's', 'm': 'd', 'n': 'f', 'o': 'g',
+        'p': 'h', 'q': 'j', 'r': 'k', 's': 'l', 't': 'z',
+        'u': 'x', 'v': 'c', 'w': 'v', 'x': 'b', 'y': 'n',
+        'z': 'm', ' ': ' '
+    }
+    encoded_message = ''
+    for i, char in enumerate(message):
+        if char in qwerty_map:
+            shift = i + 1
+            qwerty_pos = list(qwerty_map.keys()).index(char)
+            shifted_pos = (qwerty_pos + shift) % len(qwerty_map)
+            encoded_message += qwerty_map[list(qwerty_map.keys())[shifted_pos]]
+        else:
+            encoded_message += char
+    return encoded_message
+
+def qwerty_positional_decode(encoded_message):
+    qwerty_map = {
+        'q': 'a', 'w': 'b', 'e': 'c', 'r': 'd', 't': 'e',
+        'y': 'f', 'u': 'g', 'i': 'h', 'o': 'i', 'p': 'j',
+        'a': 'k', 's': 'l', 'd': 'm', 'f': 'n', 'g': 'o',
+        'h': 'p', 'j': 'q', 'k': 'r', 'l': 's', 'z': 't',
+        'x': 'u', 'c': 'v', 'v': 'w', 'b': 'x', 'n': 'y',
+        'm': 'z', ' ': ' '
+    }
+    decoded_message = ''
+    for i, char in enumerate(encoded_message):
+        if char in qwerty_map:
+            shift = i + 1
+            qwerty_pos = list(qwerty_map.keys()).index(char)
+            shifted_pos = (qwerty_pos - shift) % len(qwerty_map)
+            decoded_message += qwerty_map[list(qwerty_map.keys())[shifted_pos]]
+        else:
+            decoded_message += char
+    return decoded_message
 
 def receive():
     while True:
@@ -12,16 +57,23 @@ def receive():
             if message == 'NICK':
                 client.send(nickname.encode())
             else:
+                message = qwerty_positional_decode(message)
                 print(message)
+            logging.info('message sent')
+        except Exception as e:
+            logging.error("Exception occurred", exc_info=True)
         except:
             print("Error!")
+            logging.warning('message couldn´t be sent')
             client.close()
             break
 
 def write():
     while True:
         message = f'{nickname}: {input()}'
+        message = qwerty_positional_encode(message)
         client.send(message.encode())
+        
 
 receive_thread = threading.Thread(target = receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socket
 import threading
+import logging
 
+logging.basicConfig(filename='client.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
 host = '127.0.0.1'
 port = 50000
+
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((host,port))
@@ -10,6 +14,46 @@ server.listen()
 
 clients = []
 nicks = []
+
+def qwerty_positional_encode(message):
+    qwerty_map = {
+        'a': 'q', 'b': 'w', 'c': 'e', 'd': 'r', 'e': 't',
+        'f': 'y', 'g': 'u', 'h': 'i', 'i': 'o', 'j': 'p',
+        'k': 'a', 'l': 's', 'm': 'd', 'n': 'f', 'o': 'g',
+        'p': 'h', 'q': 'j', 'r': 'k', 's': 'l', 't': 'z',
+        'u': 'x', 'v': 'c', 'w': 'v', 'x': 'b', 'y': 'n',
+        'z': 'm', ' ': ' '
+    }
+    encoded_message = ''
+    for i, char in enumerate(message):
+        if char in qwerty_map:
+            shift = i + 1
+            qwerty_pos = list(qwerty_map.keys()).index(char)
+            shifted_pos = (qwerty_pos + shift) % len(qwerty_map)
+            encoded_message += qwerty_map[list(qwerty_map.keys())[shifted_pos]]
+        else:
+            encoded_message += char
+    return encoded_message
+
+def qwerty_positional_decode(encoded_message):
+    qwerty_map = {
+        'q': 'a', 'w': 'b', 'e': 'c', 'r': 'd', 't': 'e',
+        'y': 'f', 'u': 'g', 'i': 'h', 'o': 'i', 'p': 'j',
+        'a': 'k', 's': 'l', 'd': 'm', 'f': 'n', 'g': 'o',
+        'h': 'p', 'j': 'q', 'k': 'r', 'l': 's', 'z': 't',
+        'x': 'u', 'c': 'v', 'v': 'w', 'b': 'x', 'n': 'y',
+        'm': 'z', ' ': ' '
+    }
+    decoded_message = ''
+    for i, char in enumerate(encoded_message):
+        if char in qwerty_map:
+            shift = i + 1
+            qwerty_pos = list(qwerty_map.keys()).index(char)
+            shifted_pos = (qwerty_pos - shift) % len(qwerty_map)
+            decoded_message += qwerty_map[list(qwerty_map.keys())[shifted_pos]]
+        else:
+            decoded_message += char
+    return decoded_message
 
 def broadcast(message):
     for client in clients:
@@ -26,20 +70,25 @@ def handle(client):
             client.close()
             nick = nicks[index]
             nick.remove(nick)
-            broadcast(f"{nick} has been BANNED.".encode())
+            notification = f"{nick} is gonne."
+
+            #notification = qwerty_positional_encode(notification)
+            broadcast(notification.encode())
             break
 
 def receive():
     while (True):
         client, address = server.accept()
         print(f"Connect with address {str(address)}")
-        client.send('NICK'.encode())
+        nick_request = 'NICK'
+        client.send(nick_request.encode())
         nickname = client.recv(1024).decode()
         nicks.append(nickname)
         clients.append(client)
         print(f"Nickname of the client is {nickname}")
-        broadcast(f"{nickname} join the chat.".encode())
-        client.send("You are connected to the chat".encode())
+
+        notification = f"{nickname} join the chat."
+        broadcast(notification.encode())
 
         thread = threading.Thread(target = handle, args = (client,))
         thread.start()
